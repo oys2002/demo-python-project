75dcf90405f987d62b51789f1e64e1e6f2665070
Yamini <Yaminisaraswathi.oggu@gmail.com>
2025-02-28 16:33:41 +0530

    Added the simple py program

---
+++ file1.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum_result = num1 + num2
+
+# Print the result
+print("The sum is:", sum_result)
