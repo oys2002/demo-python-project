7c3046b6a43736de12ba300094ebdc36387c611e
Yamini <Yaminisaraswathi.oggu@gmail.com>
2025-03-05 16:55:25 +0530

    Changes done in develop branch

--- file1.py
+++ file1.py
@@ -6,3 +6,8 @@ sum_result = num1 + num2
 
 # Print the result
 print("The sum is:", sum_result)
+
+if sum_result % 2 == 0:
+    print(sum_result, "is even")
+else:
+    print(sum_result, "is odd")
--- sampleprj.py
+++ sampleprj.py
@@ -1,1 +1,3 @@
 print("Hello world")
+
+print("This is the sample program for testing the branches in the git")
