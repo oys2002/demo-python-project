61c574b30efc43c6c0303285658e1287cf1c6fbe
Yamini <Yaminisaraswathi.oggu@gmail.com>
2025-03-06 14:52:37 +0530

    changes done for math module in another branch

--- sampleprj.py
+++ sampleprj.py
@@ -1,3 +1,14 @@
 print("Hello world")
 
 print("This is the sample program for testing the branches in the git")
+
+
+
+def sqrt_newton(n, precision=0.00001):
+    guess = n / 2  # Initial guess
+    while abs(guess * guess - n) > precision:
+        guess = (guess + n / guess) / 2
+    return guess
+
+num = 25
+print(sqrt_newton(num))  # Output: 5.0
